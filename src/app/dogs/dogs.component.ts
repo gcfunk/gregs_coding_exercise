cd84c434d94ca3b49bc669e5aa2513d80a935878
Greg Funk <gregory.c.funk@gmail.com>
2022-07-31 06:07:56 -0400

    Shows a list of dogs as links

--- src/app/dogs/dogs.component.ts
+++ src/app/dogs/dogs.component.ts
@@ -3,8 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import Utils from '../utils';
 
 interface Dog {
-  breed: string;
-  image: string;
+  breed?: string;
+  image?: string;
 }
 
 @Component({
@@ -19,6 +19,7 @@ export class DogsComponent implements OnInit {
   breeds: Array<string> = [];
   numberOfDogsToShow = 11;
   images: Array<Dog> = [];
+  selectedDog: Dog = {};
 
   constructor(public http: HttpClient) { }
 
@@ -42,6 +43,9 @@ export class DogsComponent implements OnInit {
           }
           this.breeds = shortList;
 
+          // there's definitely a better way of calling a bunch of APIs at the same time
+          // particularly because if one fails the error message won't be shown
+          // I could use forkJoin or something, but this works for demonstration purposes
           this.breeds.forEach(breed => {
             this.loadImageForBreed(breed);
           });
@@ -64,10 +68,14 @@ export class DogsComponent implements OnInit {
       next: (response: any) => {
         if (response['status'] === 'success') {
           let images = response['message'];
-          this.images.push({
+          const dog = {
             breed: breed,
             image: images[Math.floor(Math.random() * images.length)]
-          });
+          };
+          this.images.push(dog);
+          if (this.images.length === 1) {
+            this.selectedDog = dog;
+          }
           this.showLoading = false;
         } else {
           this.showLoading = false;
